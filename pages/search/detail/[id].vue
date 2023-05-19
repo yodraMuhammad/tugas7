fcd1af1e47109e6142682283135f6d5758077af9
Yodra Muhamad Akbar N <yodra.muhamad@gmail.com>
2023-05-19 16:32:38 +0700

    fix bug

--- pages/search/detail/[id].vue
+++ pages/search/detail/[id].vue
@@ -19,12 +19,6 @@ console.log(data._rawValue)
         <div class="md:w-2/3">
           <h1 class="text-4xl mb-6">{{ film.Title }}</h1>
           <table class="border-collapse">
-            <!-- <thead>
-              <tr>
-                <th class="p-2">Kolom 1</th>
-                <th class="p-2">Kolom 2</th>
-              </tr>
-            </thead> -->
             <tbody>
               <tr>
                 <td class="p-2 align-top">Rilis</td>
